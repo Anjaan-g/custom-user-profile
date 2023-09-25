5985db4ff3b62a92e783691451006e898cf64b38
Anjaan <anjaangaire@gmail.com>
2023-09-25 09:25:09 +0545

    feat: pre commit hooks updated

---
+++ commit-msg.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+ARGS=$1
+COMMIT_MSG=`head -n1 $ARGS`
+PATTERN="^(feat|bugfix|ci|docs|settings|template):+[[:space:]]+[a-z]"
+if ! [[ "$COMMIT_MSG" =~ $PATTERN ]]; then
+    echo "Bad commit message '$COMMIT_MSG'. It must be in the format of 'feat: message' or 'bugfix: message' or
+    'ci: message' or 'docs: message' or 'settings: message' or 'template: message' "
+    exit 1
+fi
