139c648ae96da8593d3839fa70e244410a6ad8c8
Anjaan <anjaangaire@gmail.com>
2023-09-26 09:04:44 +0545

    build: version updated

--- commit-msg.sh
+++ commit-msg.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ARGS=$1
 COMMIT_MSG=`head -n1 $ARGS`
-PATTERN="^(feat|bugfix|ci|docs|settings|template):+[[:space:]]+[a-z]"
+PATTERN="^(feat|bugfix|ci|docs|settings|template|build):+[[:space:]]+[a-z]"
 if ! [[ "$COMMIT_MSG" =~ $PATTERN ]]; then
     echo "Bad commit message '$COMMIT_MSG'. It must be in the format of 'feat: message' or 'bugfix: message' or
     'ci: message' or 'docs: message' or 'settings: message' or 'template: message' "
